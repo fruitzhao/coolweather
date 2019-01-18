c303ba14ad6d68962c264fbc7e6dfcaacd1cc28b
zhaofei <fiezhao@gmail.com>
2019-01-18 15:28:27 +0800

    添加定位功能。

--- app/src/main/java/com/coolweather/android/MainActivity.java
+++ app/src/main/java/com/coolweather/android/MainActivity.java
@@ -1,24 +1,84 @@
 package com.coolweather.android;
 
+import android.Manifest;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.preference.PreferenceManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
 import com.coolweather.android.gson.Weather;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+
+    public static boolean isLoc = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
-        if (prefs.getString("weather", null) != null) {
-            Intent intent = new Intent(this, WeatherActivity.class);
-            startActivity(intent);
-            finish();
+        List<String> permissionList = new ArrayList<>();
+        if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.
+                permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            permissionList.add(Manifest.permission.ACCESS_FINE_LOCATION);
+        }
+        if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.
+                permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED) {
+            permissionList.add(Manifest.permission.READ_PHONE_STATE);
+        }
+        if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.
+                permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            permissionList.add(Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        }
+        if (!permissionList.isEmpty()) {
+            String [] permissions = permissionList.toArray(new String[permissionList.size()]);
+            ActivityCompat.requestPermissions(MainActivity.this, permissions, 1);
+        } else {
+            initWeather();
         }
     }
+
+    /**
+     * 处理权限
+     */
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case 1:
+                if (grantResults.length > 0) {
+                    for (int result : grantResults) {
+                        if (result != PackageManager.PERMISSION_GRANTED) {
+                            Toast.makeText(this, "必须同意所有权限才能使用本程序",
+                                    Toast.LENGTH_SHORT).show();
+                            finish();
+                            return;
+                        }
+                    }
+                    initWeather();
+                } else {
+                     Toast.makeText(this, "发生未知错误", Toast.LENGTH_SHORT);
+                     finish();
+                }
+                break;
+                default:
+        }
+    }
+
+    private void initWeather() {
+        Intent intent = new Intent(this, WeatherActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+
 }
--- app/src/main/java/com/coolweather/android/WeatherActivity.java
+++ app/src/main/java/com/coolweather/android/WeatherActivity.java
@@ -1,24 +1,33 @@
 package com.coolweather.android;
 
+import android.Manifest;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.graphics.Color;
 import android.os.Build;
 import android.preference.PreferenceManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.ProgressBar;
 import android.widget.ScrollView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.baidu.location.BDAbstractLocationListener;
+import com.baidu.location.BDLocation;
+import com.baidu.location.LocationClient;
 import com.bumptech.glide.Glide;
 import com.coolweather.android.gson.Forecast;
 import com.coolweather.android.gson.LifeStyle;
@@ -28,16 +37,28 @@ import com.coolweather.android.util.HttpUtil;
 import com.coolweather.android.util.Utility;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import okhttp3.Call;
 import okhttp3.Callback;
 import okhttp3.Response;
+
+import static com.coolweather.android.MainActivity.isLoc;
 
 public class WeatherActivity extends AppCompatActivity {
 
     public DrawerLayout drawerLayout;
 
     private Button navButton;
+
+    private Button locButton;
+
+    private LocationClient mLocationClient;
+
+    private String myLocation;
 
     public SwipeRefreshLayout swipeRefresh;
 
@@ -59,10 +80,13 @@ public class WeatherActivity extends AppCompatActivity {
 
     private ImageView bingPicImg;
 
+    private ProgressBar progressBar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (Build.VERSION.SDK_INT >=21) {
+        //状态栏透明，填充软件内容
+        if (Build.VERSION.SDK_INT >= 24) {
             View decorView = getWindow().getDecorView();
             decorView.setSystemUiVisibility(
                     View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN |
@@ -73,6 +97,7 @@ public class WeatherActivity extends AppCompatActivity {
         //初始化各控件
         drawerLayout = findViewById(R.id.drawer_layout);
         navButton = findViewById(R.id.nav_button);
+        locButton = findViewById(R.id.loc_button);
         weatherLayout = findViewById(R.id.weather_layout);
         titleCity = findViewById(R.id.title_city);
         titleUpdateTime = findViewById(R.id.title_update_time);
@@ -81,8 +106,11 @@ public class WeatherActivity extends AppCompatActivity {
         forecastLayout = findViewById(R.id.forecast_layout);
         lifestyleLayout = findViewById(R.id.lifestyle_layout);
         bingPicImg = findViewById(R.id.bing_pic_img);
+        progressBar = findViewById(R.id.progress_bar);
         swipeRefresh =findViewById(R.id.swipe_refresh);
         swipeRefresh.setColorSchemeResources(R.color.colorPrimary);
+        mLocationClient = new LocationClient(getApplicationContext());
+        mLocationClient.registerLocationListener(new MyLocationListener());
         SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
         String weatherString = prefs.getString("weather",null);
         if (weatherString != null) {
@@ -90,6 +118,15 @@ public class WeatherActivity extends AppCompatActivity {
             Weather weather = Utility.handWeatherResponse(weatherString);
             mWeatherId = weather.basic.weatherId;
             showWeatherInfo(weather);
+        }
+
+        //首次进入应用自动刷新展示当前地址的天气
+        if (!isLoc) {
+            loadBingPic();
+            progressBar.setVisibility(View.VISIBLE);
+            requestLocation();
+            progressBar.setVisibility(View.GONE);
+            isLoc = true;
         } else {
             //无缓存时去服务器查询天气
             mWeatherId = getIntent().getStringExtra("weather_id");
@@ -114,7 +151,17 @@ public class WeatherActivity extends AppCompatActivity {
                 drawerLayout.openDrawer(GravityCompat.START);
             }
         });
+
+        locButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                progressBar.setVisibility(View.VISIBLE);
+                requestLocation();
+                progressBar.setVisibility(View.GONE);
+            }
+        });
     }
+
 
     /**
      *根据天气id去服务器查询天气信息
@@ -130,7 +177,7 @@ public class WeatherActivity extends AppCompatActivity {
                 runOnUiThread(new Runnable() {
                     @Override
                     public void run() {
-                        Toast.makeText(WeatherActivity.this, "获取天气信息失败",
+                        Toast.makeText(WeatherActivity.this, "获取天气信息失败(error:404)",
                                 Toast.LENGTH_SHORT).show();
                         swipeRefresh.setRefreshing(false);
                     }
@@ -152,7 +199,7 @@ public class WeatherActivity extends AppCompatActivity {
                             mWeatherId = weather.basic.weatherId;
                             showWeatherInfo(weather);
                         } else {
-                            Toast.makeText(WeatherActivity.this, "获取天气信息失败",
+                            Toast.makeText(WeatherActivity.this, "获取天气信息失败(error:403)",
                                     Toast.LENGTH_SHORT).show();
                         }
                         swipeRefresh.setRefreshing(false);
@@ -195,12 +242,14 @@ public class WeatherActivity extends AppCompatActivity {
      * 处理并展示Weather实体类中的数据
      */
     private void showWeatherInfo(Weather weather) {
+        forecastLayout.setVisibility(View.VISIBLE);
+        lifestyleLayout.setVisibility(View.VISIBLE);
         String cityName = weather.basic.cityName;
         String updateTime = weather.update.updateTime.split(" ")[1];
         String degree = weather.now.temperature + "℃";
         String weatherInfo = weather.now.info;
         titleCity.setText(cityName);
-        titleUpdateTime.setText(updateTime);
+        titleUpdateTime.setText("更新于 " + updateTime);
         degreeText.setText(degree);
         weatherInfoText.setText(weatherInfo);
         forecastLayout.removeAllViews();
@@ -219,9 +268,9 @@ public class WeatherActivity extends AppCompatActivity {
         lifestyleLayout.removeAllViews();
         for (LifeStyle lifeStyle : weather.lifeStyleList) {
             View view = LayoutInflater.from(this).inflate(R.layout.lifestyle_item, lifestyleLayout, false);
-            //TextView briefText = view.findViewById(R.id.lifestyle_brief_text);
+            TextView briefText = view.findViewById(R.id.lifestyle_brief_text);
             TextView infoText = view.findViewById(R.id.lifestyle_info_text);
-            //briefText.setText(lifeStyle.brief);
+            briefText.setText(lifeStyleType(lifeStyle.type) + " : " + lifeStyle.brief);
             infoText.setText(lifeStyle.info);
             lifestyleLayout.addView(view);
         }
@@ -229,4 +278,51 @@ public class WeatherActivity extends AppCompatActivity {
         Intent intent = new Intent(this, AutoUpdateService.class);
         startService(intent);
     }
+
+    private void requestLocation() {
+        mLocationClient.start();
+    }
+
+    /**
+     * 定位监听器
+     */
+    public class MyLocationListener extends BDAbstractLocationListener {
+
+        @Override
+        public void onReceiveLocation(BDLocation location) {
+            myLocation = location.getLongitude() + "," + location.getLatitude();
+            requestWeather(myLocation);
+            mLocationClient.stop();
+        }
+
+    }
+
+    /**
+     * 建立字典转换生活建议类型
+     */
+    private static String lifeStyleType(String type) {
+        Map<String, String> map = new HashMap<>();
+        map.put("comf","舒适度指数");
+        map.put("cw","洗车指数");
+        map.put("drsg","穿衣指数");
+        map.put("flu","感冒指数");
+        map.put("sport","运动指数");
+        map.put("trav","旅游指数");
+        map.put("uv","紫外线指数");
+        map.put("air","空气污染扩散条件指数");
+        map.put("ac","空调开启指数");
+        map.put("ag","过敏指数");
+        map.put("gl","太阳镜指数");
+        map.put("mu","化妆指数");
+        map.put("airc","晾晒指数");
+        map.put("ptfc","交通指数");
+        map.put("fsh","钓鱼指数");
+        map.put("spi","防晒指数");
+        if (map.get(type) != null) {
+            return map.get(type);
+        } else {
+            return type;
+        }
+    }
+
 }
